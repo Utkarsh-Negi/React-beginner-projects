2c7f0339cbf9eb659a0845480f86dc1317e88e26
Utkarsh <utkarshnegi25@gmail.com>
2021-07-21 00:52:17 +0530

    Final commit

--- THA-18,19 inline-css/src/component/button.jsx
+++ THA-18,19 inline-css/src/component/button.jsx
@@ -3,7 +3,12 @@ import React, { useState } from 'react';
 function Button() {
   const [counter, setCounter] = useState(0);
 
-  return <span onClick={() => setCounter(counter + 1)}> {counter} </span>;
+  return (
+    <span id="butt" onClick={() => setCounter(counter + 1)}>
+      {' '}
+      {counter}{' '}
+    </span>
+  );
 }
 
 export default Button;
