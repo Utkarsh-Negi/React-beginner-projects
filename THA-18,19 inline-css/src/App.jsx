6a9ed0c18da41cca63b2329f829ba054694800ab
Utkarsh <utkarshnegi25@gmail.com>
2021-07-21 00:16:40 +0530

    Added and styled chess board component

---
+++ THA-18,19 inline-css/src/App.jsx
@@ -0,0 +1,22 @@
+import Chess from './component/Chess';
+import Button from './component/button';
+
+function App() {
+  return (
+    <div>
+      <Chess />
+      <div className="State">
+        <p>
+          There are 4 counter component instances that each manage their own
+          state.
+        </p>
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default App;
