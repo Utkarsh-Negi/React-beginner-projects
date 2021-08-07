6095c898e7e55b7d688298891efa641dccbdc0bb
Utkarsh <utkarshnegi25@gmail.com>
2021-08-07 12:47:09 +0530

    Completed all the navbar components

---
+++ router-app/src/components/dashboard.jsx
@@ -0,0 +1,8 @@
+function Dash() {
+  return (
+    <div className="dash">
+      <h2>Dashboard:Private Route</h2>
+    </div>
+  );
+}
+export default Dash;
---
+++ router-app/src/components/home.jsx
@@ -0,0 +1,16 @@
+function Home(login, setLogin) {
+  return (
+    <div className="home">
+      <div className="header">
+        <h1>Home</h1>
+        <p>If not logged in you cant access, Profile & Dashboard </p>
+        {login ? (
+          <button onClick={() => setLogin(false)}>Logout</button>
+        ) : (
+          <button onClick={() => setLogin(true)}>Login</button>
+        )}
+      </div>
+    </div>
+  );
+}
+export default Home;
